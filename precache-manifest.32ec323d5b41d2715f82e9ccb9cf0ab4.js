31321abb7fbd8bf2d36f9e469e21e2f3ea7c39f9
ArwinChua01 <36938961+ArwinChua01@users.noreply.github.com>
2019-02-08 10:50:12 +0800

    Add files via upload

---
+++ precache-manifest.32ec323d5b41d2715f82e9ccb9cf0ab4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ebd8a7e547e797fc3113",
+    "url": "/static/js/main.ebd8a7e5.chunk.js"
+  },
+  {
+    "revision": "5a50fd3bdc385a9f7eee",
+    "url": "/static/js/1.5a50fd3b.chunk.js"
+  },
+  {
+    "revision": "ebd8a7e547e797fc3113",
+    "url": "/static/css/main.5c52d841.chunk.css"
+  },
+  {
+    "revision": "cf72ad973664da21a92fd04b111ca828",
+    "url": "/index.html"
+  }
+];
